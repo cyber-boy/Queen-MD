d5ed83eaf4ad36fe7654c9bcb98b8f55e5ad0b09
cyber-boy <119953217+cyber-boy@users.noreply.github.com>
2023-03-18 09:52:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,8 +22,8 @@ global.APIKeys = {
 ////////////////////////////////////////////////////|:|             OWNER DETAILS             |:|//////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 global.session = process.env.SESSION_ID || 'PUT HERE',
-global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
+global.ownername = process.env.OWNER_NAME === undefined ? "SonuJod" : process.env.OWNER_NAME
+let fake = process.env.OWNER_NUMBER === undefined ? '9495216595' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
